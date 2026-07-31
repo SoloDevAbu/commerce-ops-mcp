cae7c150dc0ccf59a4162aa195be7db72c02a1ff
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-07-31 15:09:31 +0530

    feat: add we a dependency and modify client to establish WebSocket connections for seeding db

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 export const db = drizzle({ client: pool });
